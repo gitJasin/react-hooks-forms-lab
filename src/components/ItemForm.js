89848507a3faed7bcc7aaac160c48563b680441a
Jason Stewart <jaysin.stewart@gmail.com>
2023-06-09 09:06:09 -0700

    Got data from submit form

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
 function ItemForm(props) {
+  const [formData, setformData] = useState({
+    name: "",
+    category: "Produce"
+  })
+  
+  function handleSubmit (e) {
+    e.preventDefault()
+    console.log(formData)
+  }
+
+  function handleChange (e) {
+    const name = e.target.name
+    const value = e.target.value
+    setformData({...formData, [name]: value})
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}> 
       <label>
         Name:
-        <input type="text" name="name" />
+        <input 
+          type="text" 
+          name="name" 
+          onChange={handleChange}
+          value={formData.name}
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select 
+          name="category"
+          onChange={handleChange}
+          value={formData.category}
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
